5d1bd50e4fdcc652510e690a645b1c9f2beeafd4
Pedro Amaral <pedro.martines.amaral@gmail.com>
2023-03-30 15:39:57 -0300

    sugestões de mudança
    - Adicionado atributo required nos inputs;
    - Atributos de acessibilidade para imagem da logo;
    - Algumas mudanças nos nomes de classes.
    - Border-radius no botão entrar
    - Aumento da fonte dos label
    - Cor do "manager" direto na tag span

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,119 +1,118 @@
+<script>
+    export default {
+        name: 'Login'
+    }
+</script>
+
 <template>
     <div class="login">
-        <div class="container form-logo align-middle">
-            <div class="logo-nome mb-2">
-                <img src="../assets/img/task-manager-logo.png" width="50" height="50"
-                    class="logo-tm d-inline-block align-top align-middle">
-                <span class="nome-tm align-middle">Task<span class="manager">Manager</span></span>
+        <div class="container align-middle">
+            <div class="logo mb-2">
+                <img src="../assets/img/task-manager-logo.png" width="50" height="50" 
+                     class="d-inline-block align-top align-middle" aria-labelledby="task-manager" />
+                <p class="logo-type align-middle" id="task-manager">Task <span style="color: #fff">Manager</span></p>
             </div>
-            <form class="formulario-login mb-5">
+
+            <form class="login-form mb-5">
                 <div class="input-container">
                     <label for="email">E-mail</label>
                     <input type="email" class="form-control" id="email" name="email" v-model="email"
-                        placeholder="Digite o seu email">
+                           placeholder="Digite o seu email" required />
                 </div>
+                
                 <div class="input-container">
                     <label for="senha">Senha</label>
                     <input type="password" class="form-control" id="senha" name="senha" v-model="senha"
-                        placeholder="Digite a sua senha">
+                           placeholder="Digite a sua senha" required />
                 </div>
+
                 <div class="input-container">
                     <button class="btn-entrar" type="submit">ENTRAR</button>
                 </div>
-                <span class="para-cadastrar">Ainda não está cadastrado? <router-link to="/cadastroUsuario"
-                        class="link-registrar">Clique aqui e
-                        registre-se.</router-link></span>
+                <span class="cadastre-se">
+                    <router-link to="/cadastroUsuario" class="link-cadastro">
+                        Não possui cadastro?<br />
+                        Crie sua conta
+                    </router-link>
+                </span>
             </form>
-        </div>
+        </div><!-- div container -->
     </div>
 </template>
 
-<script>
-
-export default {
-    name: 'Login'
-}
-
-</script>
-
 <style scoped>
-
-.manager {
-    color: #fff;
-}
-.para-cadastrar {
-    font-size: 12px;
-    justify-content: center;
-}
-
-.logo-tm {
-    margin-right: 5px;
-}
-
-.nome-tm {
-    color: #13678A;
-    font-size: 30px;
-}
-
-.logo-nome {
-    text-align: center;
-}
-
-.link-registrar {
-    color: #13678A;
-    font-weight: bold;
-}
-
-.login {
-    background-color: #0B384F;
-    height: 100vh;
-    width: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-}
-
-.formulario-login {
-    background-color: #fff;
-    padding: 20px;
-    padding-top: 50px;
-    border-radius: 4px;
-    max-width: 400px;
-    margin: 0 auto;
-}
-
-.input-container {
-    display: flex;
-    flex-direction: column;
-    margin-bottom: 20px;
-}
-
-label {
-    font-weight: bold;
-    margin-bottom: 5px;
-    color: #012030;
-}
-
-input {
-    background-color: #E7E7E7;
-    border: none;
-    box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.3);
-}
-
-.btn-entrar {
-    font-weight: bold;
-    padding: 10px;
-    margin-top: 40px;
-    color: #fff;
-    background-color: #012030;
-    border: none;
-    box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.3);
-}
-
-@media only screen and (max-width: 500px) {
-    .form-logo {
-        width: 90%;
-        padding: 20px;
+    .login {
+        background-color: #0B384F;
+        height: 100vh;
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
     }
-}
+
+    div .logo {
+        text-align: center;
+    } 
+
+    .logo-type {
+        color: #13678A;
+        font-size: 30px;
+        margin-right: 5px;
+        display: inline;
+    }
+
+    .login-form {
+        background-color: #fff;
+        padding: 50px 20px 20px;
+        max-width: 400px;
+        margin: 0 auto;
+        border-radius: 4px;
+    }
+
+    .input-container {
+        display: flex;
+        flex-direction: column;
+        margin-bottom: 20px;
+    }
+
+    .btn-entrar {
+        font-weight: bold;
+        padding: 10px;
+        margin-top: 30px;
+        color: #fff;
+        background-color: #012030;
+        border: none;
+        box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.3);
+        border-radius: 4px;
+    }
+
+    span .cadastre-se {
+        font-size: 12px;
+        justify-content: center;
+    }
+
+    .link-cadastro {
+        color: #13678A;
+        font-weight: bold;
+    }
+
+    label {
+        font-size: 1.1em;
+        font-weight: bold;
+        margin-bottom: 5px;
+        color: #012030;
+    }
+
+    input {
+        background-color: #E7E7E7;
+        border: none;
+        box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.3);
+    }
+
+    @media only screen and (max-width: 500px) {
+        .form-logo {
+            width: 90%;
+            padding: 20px;
+        }
+    }
 </style>
